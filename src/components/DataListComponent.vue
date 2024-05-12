8f47261b11c6a06b37c9af724b13abcb50152299
MikhailPermiakov <mp9512056251@gmail.com>
2024-05-12 23:36:21 +0300

    start project

---
+++ src/components/DataListComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>hello{{ name }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+
+  setup(){
+    const name = ref<string>('Mikhail')
+
+    return {
+      name,
+    }
+  }
+})
+</script>
