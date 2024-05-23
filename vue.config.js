dce0039e7a7f0460a6704aded574134385c48cc3
MikhailPermiakov <mp9512056251@gmail.com>
2024-05-23 03:11:15 +0300

    add deploy.sh 23.05.24

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/test-for-befit/'
-      : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/test-for-befit/' : '/',
   transpileDependencies: true
 })
