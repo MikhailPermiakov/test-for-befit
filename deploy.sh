a780402e733e003aa4f57b03a86bc5fbbaf7eb03
MikhailPermiakov <114807388+MikhailPermiakov@users.noreply.github.com>
2024-05-24 01:39:54 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,45 @@
-# #!/usr/bin/env sh
+#!/usr/bin/env sh
 
-# # остановить публикацию при ошибках
-# set -e
+# остановить публикацию при ошибках
+set -e
 
-# # сборка
-# npm run build
+# сборка
+npm run build
 
-# # переход в каталог сборки
-# cd dist
+# переход в каталог сборки
+cd dist
 
-# # если вы публикуете на пользовательский домен
-# # echo 'www.example.com' > CNAME
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
 
-# git init
-# git add -A
-# git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
-# # если вы публикуете по адресу https://<USERNAME>.github.io
-# # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# если вы публикуете по адресу https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 
 
-# abort on errors 
-set -e 
+# # abort on errors 
+# set -e 
 
-# build 
-echo Linting.. 
-npm run lint 
-echo Building. this may take a minute... 
-npm run build 
+# # build 
+# echo Linting.. 
+# npm run lint 
+# echo Building. this may take a minute... 
+# npm run build 
 
-# if you are deploying to a custom domain add a CNAME (uncomment the next 3 lines) 
-#cd docs 
-#echo 'yourcustomdomain.com' > CNAME 
-#cd - 
+# # if you are deploying to a custom domain add a CNAME (uncomment the next 3 lines) 
+# #cd docs 
+# #echo 'yourcustomdomain.com' > CNAME 
+# #cd - 
 
-# deploy 
-echo Deploying.. 
-git add -A 
-git commit -m 'deploy' 
+# # deploy 
+# echo Deploying.. 
+# git add -A 
+# git commit -m 'deploy' 
 
 
  git push -f https://mikhailpermiakov.github.io/test-for-befit/
